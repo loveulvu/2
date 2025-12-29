bf7f19a9aab461634a598c5e685f97e737328f5a
loveulvu <412401474@qq.com>
2025-12-29 20:57:47 +0800

    support command line argument

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import sys
+
 def greet(name):
     return f"hello, {name}"
 
 if __name__ == "__main__":
-    print(greet("world"))
+    if len(sys.argv) > 1:
+        print(greet(sys.argv[1]))
+    else:
+        print("usage: python main.py <name>")
